00de72572b7a8b607d64d132ff4786fb020fa9e7
Christophe VG <contact@christophe.vg>
2018-07-29 16:04:02 +0000

    extracted main from router.js

--- vue/static/router.js
+++ vue/static/router.js
@@ -7,24 +7,3 @@ var router = new VueRouter({
   mode  : 'history'
 });
 
-var app = new Vue({
-  el: "#app",
-  delimiters: ['${', '}'],
-  router: router,
-  components: {},
-  data: {
-    drawer: null,
-    sections: [
-      { icon: "home", text: "Home", path: "/" },
-    ]
-  },
-  methods: {
-    // VFG background colors clash
-    fixVuetifyCSS : function() {
-      this.$vuetify.theme.info  = '#ffffff';
-      this.$vuetify.theme.error = '#ffffff';
-    }
-  }
-}).$mount('#app');
-
-app.fixVuetifyCSS();
